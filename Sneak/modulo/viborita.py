facb90090310bc94f054b551d916d45fa1ce56fb
Elias-C0 <elias_prueba@hotmail.com>
2023-12-18 21:01:40 -0300

    carpetas

---
+++ Sneak/modulo/viborita.py
@@ -0,0 +1,8 @@
+class Viborita:
+    def __init__(self,cabeza,cola,nivel=0):
+        self.nivel= nivel
+        self.cabeza= cabeza
+        self.cola= cola
+
+    def longitud(self,nivel):
+        pass
